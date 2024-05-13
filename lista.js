ed59fece525441c7acfbec25cc597454671e9323
larissa vitoria mentges <107115194+lari12345@users.noreply.github.com>
2024-05-13 09:11:09 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('#Lista-itens li').forEach(function(itens)
+{Item.addEventListener('click', function(){
+    document.getElementById('mensagem-item'). textContent= tris.getattribute
+    ('data-mensagem');})
+});
