94499bfbf443562c602a9b77a08f4fd3454d2255
larissa vitoria mentges <107115194+lari12345@users.noreply.github.com>
2024-06-10 09:13:06 -0300

    Add files via upload

--- lista.js
+++ lista.js
@@ -1,5 +1,18 @@
 document.querySelectorAll('#Lista-itens li').forEach(function(itens)
 {Item.addEventListener('click', function(){
-    document.getElementById('mensagem-item'). textContent= tris.getattribute
-    ('data-mensagem');})
+    document.getElementById('mensagem-item'). textContent= this.getAttribute
+    ('data-mensagem');
+});
+
+document.querySelectorAll('#Lista-itens li').forEach(function(item)
+{Item.addEventListener('click', function(){
+    document.getElementById('mensagem-item'). textContent= this.getAttribute
+    ('data-mensagem');
+
+    document.getElementById('mensagem-item'). src = this.getAttribute ('data-mensagem');
+    document.getElementById('mensagem-item'). style.display= 'block';
+    document.getElementById('descrição-item'). textContent= this.getAttribute
+    ('data-descrição');})
+    
+});
 });
